ddb555103044391771681d6ad560ad20b35f8b48
Francesco1204 <58953468+Francesco1204@users.noreply.github.com>
2020-05-07 15:49:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const eventi = [
 
   {"evento": "C'è un'infiltrazione di acqua sporca, vuoi berla?",
   "risultato": "Non è stata un'idea saggissima, ma almeno hai meno sete",
-  "Salute": -10, "Sete": 20, "Fame": 0, "Radio": 0},
+  "Salute": -5, "Sete": 24, "Fame": 0, "Radio": 0},
 
   {"evento": "Un topo si è nascosto nel bunker, snack?", 
   "risultato": "Non è esattamente il tipo di snack che sponsorizzerebbe un atleta... ti ha sfamato, ma ora stai uno schifo.",
@@ -42,7 +42,30 @@ const eventi = [
   {"evento": "Sono venute a prelevarti delle persone che ti promettono la salvezza, vuoi andare con loro?",
   "risultato": "Cretino, mai fidarsi degli sconosciuti! Infatti in realtà sono alieni che ti hanno rapito per studiarti. Sei destinato ad una vita di torture strazianti, ed una morte lunga e infelice. Ah e quasi dimenticavo, TU PARTORIRAI CON DOLORE.",
   "Salute": 0, "Sete": 0, "Fame": 0, "Radio": 0}
-  ]
+]
+
+funFacts = [
+  {"immagine": "https://ak1.picdn.net/shutterstock/videos/3159301/thumb/9.jpg", 
+  "fatto": "Fidarsi è bene, ma non fidarsi è meglio... Non sempre aprire la porta a degli sconosciuti è una buona idea."},
+
+  {"immagine": "https://adozioneadistanza.actionaid.it/magazine/wp-content/uploads/2018/04/malattie-mancanza-acqua.jpeg", 
+  "fatto": "L'acqua non potabile potrebbe causare febbre, epatite e disordini gastrointestinali."},
+
+  {"immagine": "https://www.kindpng.com/picc/m/131-1316525_clip-art-cockroach-drawing-oggy-drawing-step-by.png", 
+  "fatto": "Gli scarafaggi sono così tanto delle bestie di Satana, che neanche un fallout nucleare li ucciderebbe..."},
+
+  {"immagine": "https://thumbs-prod.si-cdn.com/yG9DSYOVI4uGhYyvoKhvW1RGtik=/fit-in/1600x0/https://public-media.si-cdn.com/filer/01/fb/01fb1828-7c3e-4a54-8228-a9dc21fbcaf8/waterglass_edit.jpg", 
+  "fatto": "Secondo uno studio dell'università di Campeggine tutte le persone che sono morte hanno bevuto acqua nell'ultima settimana, sta attento!"},
+  
+  {"immagine": "https://best5.it/b5/wp-content/uploads/2019/01/sciacallo-5-800x400-800x400.jpg", 
+  "fatto": "Non c'è limite al peggio, infatti, gli sciacalli non si arrendono mai, tanto che anche in questi periodi di crisi organizzano incursioni."},
+
+  {"immagine": "https://www.corso-antincendio.org/wp-content/uploads/2017/10/disidratazione.png", 
+  "fatto": "Senza acqua si resiste in media 10 giorni, ma attenzione a portare il fisico al limite..."},
+
+  {"immagine": "https://www.filippo-ongaro.com/hubfs/resistere-attacchi-fame.jpg", 
+  "fatto": "Mangiare un boccone ogni tanto non può far male, ma attenzione a ciò che si ingurgita."},
+]
 
 let giorno = 0
 
@@ -52,6 +75,8 @@ var barraFame = 100
 var barraRadio = 10
 
 let ran = 0
+let cas = 0
+let ces = 0
 let pulsi = false
 let victLose = true
 
@@ -70,10 +95,10 @@ function nextDay(){
     pulsi = true
 
     ran = getRandomIntInclusive(0, eventi.length)
-    while (giorno<=17 && eventi[ran].evento == "Sono venuti a prelevarti i militari, vuoi andare con loro?") {
+    while (giorno<=15 && eventi[ran].evento == "Sono venuti a prelevarti i militari, vuoi andare con loro?") {
       ran = getRandomIntInclusive(0, eventi.length)
     }
-    const element = document.getElementById("sticazzi")
+    const element = document.getElementById("paragrafoEventoQuotidiano")
     element.innerHTML = eventi[ran].evento
 
     barraSalute = barraSalute - barraRadio/10
@@ -96,21 +121,27 @@ function nextDay(){
     veliu.value = "Sì"
 
     if (barraSalute <= 0 || barraSete <= 0 || barraFame <= 0 || barraRadio > 80) {
-      document.getElementById("sticazzi").innerHTML = "Sei morto... Unlucky! https://youtu.be/CaUR7h4WunY"
-      document.getElementById("scomparire").value="Qifsha Ropt"
-      document.getElementById("COD").value="Qija me arusha"
-      
+      document.getElementById("paragrafoEventoQuotidiano").innerHTML = "Sei morto... Unlucky!"
+      document.getElementById("scomparire").value="You Died"
+      document.getElementById("COD").value="Mission Failed"
+
       pulsi = false
       victLose = false
+      
+      document.getElementById("card1").innerHTML = "La morte non fa bene, cerca di evitarla il più possibile!"
+      document.getElementById("cardImg1").src = "https://images.squarespace-cdn.com/content/v1/57ab394cbe659454809bdef9/1536080082784-N2B02QF5J5021A2PVNEJ/ke17ZwdGBToddI8pDm48kO7Rg1Gpu728H4UqxUIfecJZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PICj5SdZHY9KUswDgTH3eH8sP5PrkY15Dr7CE2CPSRiTEKMshLAGzx4R3EDFOm1kBS/skull.jpg"
+
+      document.getElementById("card2").innerHTML = "Se continui a finire qui, prova a cercare di non morire, vedrai che farai passi da gigante!"
+      document.getElementById("cardImg2").src = "https://www.taxidrivers.it/wp-content/uploads/2018/04/Taxi-Drivers_Storia-di-un-fantasma_A-Ghost-Story_David-Lowery_Luca-Biscontini_home-video.png"
+
     }
 
-    if (giorno == 1){
-      document.getElementById("inizZziazZzione").innerHTML = ""
-    }
+    imgCard()
+    
   }
 }
 
-function tastaTesticula(){
+function eventoQuotidiano(){
   if (pulsi) {
     barraSalute = barraSalute + eventi[ran].Salute 
     barraSete = barraSete + eventi[ran].Sete
@@ -128,42 +159,52 @@ function tastaTesticula(){
     if (barraSete > 0) {const marianne = document.getElementById("myBar1").style.width = barraSete + eventi[ran].Sete + "%"} 
     else {const marianne = document.getElementById("myBar1").style.width = barraSete - barraSete + "%"}
     
-    if (barraSalute > 0) {const arianne = document.getElementById("myBar2").style.width = barraFame + eventi[ran].Fame + "%"}
+    if (barraFame > 0) {const arianne = document.getElementById("myBar2").style.width = barraFame + eventi[ran].Fame + "%"}
     else {const arianne = document.getElementById("myBar2").style.width = barraFame - barraFame + "%"}
     
     if (barraRadio > 0) {const coarianne = document.getElementById("myBar3").style.width = barraRadio + eventi[ran].Radio + "%"} 
     else {const coarianne = document.getElementById("myBar3").style.width = barraRadio - barraRadio + "%"}
     
-    document.getElementById("sticazzi").innerHTML = eventi[ran].risultato 
+    document.getElementById("paragrafoEventoQuotidiano").innerHTML = eventi[ran].risultato 
     const veliu = document.getElementById("scomparire")
-    veliu.value = "Cort"
+    veliu.value = "Hai fatto la tua scelta."
     pulsi = false
 
     if (eventi[ran].evento == "Sono venuti a prelevarti i militari, vuoi andare con loro?"){
-      document.getElementById("sticazzi").innerHTML = eventi[ran].risultato
+      document.getElementById("paragrafoEventoQuotidiano").innerHTML = eventi[ran].risultato
       document.getElementById("scomparire").value = "Bravo!! Hai vinto!"
       document.getElementById("COD").value = "Campione"
       pulsi = false
       victLose = false
-      document.getElementById("morte").innerHTML = "Guarda questo fantastico video!!!!"
+      
     }
 
     if (eventi[ran].evento == "Sono venute a prelevarti delle persone che ti promettono la salvezza, vuoi andare con loro?"){
-      document.getElementById("sticazzi").innerHTML = eventi[ran].risultato
-      document.getElementById("scomparire").value = "CoG-Li0n31!1!1!1!1!i"
+      document.getElementById("paragrafoEventoQuotidiano").innerHTML = eventi[ran].risultato
+      document.getElementById("scomparire").value = "Luser!1!1!1!1!"
       document.getElementById("COD").value = "yourIQ = ForrestGumpIQ"
       pulsi = false
       victLose = false
-      document.getElementById("morte").innerHTML = "Guarda questo fantastico video!!!!"
     }
      
     if (barraSalute <= 0 || barraSete <= 0 || barraFame <= 0 || barraRadio > 80) { 
-      document.getElementById("sticazzi").innerHTML = "Hah Gayyyyyy, estas muerto!! Hey non ti arrendere, crediamo in te, ricarica la pagina e riprovaci!"
-      document.getElementById("scomparire").value="Qifsha Ropt"
-      document.getElementById("COD").value="Qija me arusha"
+      document.getElementById("paragrafoEventoQuotidiano").innerHTML = "Estàs muertooooo!! Hey non ti arrendere, crediamo in te, ricarica la pagina e riprovaci!"
+      document.getElementById("scomparire").value="You Died"
+      document.getElementById("COD").value="Mission Failed"
       pulsi = false
       victLose = false  
-      document.getElementById("morte").innerHTML = "Guarda questo fantastico video!!!!"
     }
   }
+}
+
+function imgCard(){
+  cas = getRandomIntInclusive(0, funFacts.length)
+  fatto1 = document.getElementById("pCard1")
+  fatto1.innerHTML = funFacts[cas].fatto
+  img1 = document.getElementById("cardImg1").src = funFacts[cas].immagine
+
+  ces = getRandomIntInclusive(0, funFacts.length)
+  fatto2 = document.getElementById("pCard2")
+  fatto2.innerHTML = funFacts[ces].fatto
+  img2 = document.getElementById("cardImg2").src = funFacts[ces].immagine
 }
