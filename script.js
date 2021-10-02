77dfd4296ab6aa514136d5e33ec684f1f6c9b374
Deepak Patil <72160764+Deepzoid@users.noreply.github.com>
2021-10-02 19:52:38 +0530

    Update script.js
    the values must be variables

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const button = document.getElementById("incrementa-btn")
-const counter = document.getElementById("contatore-lbl")
+var button = document.getElementById("incrementa-btn")
+var counter = document.getElementById("contatore-lbl")
 
 let counterValue = 0
 const increment = () => {
